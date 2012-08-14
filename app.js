8808e3e440922bc0787c4bad496fc90ec9821ad3
Umut Kaya Ilkiz <umutkaya2000@hotmail.com>
2012-08-14 19:22:24 +0300

    app.js send index.html file to client

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ var app = require('express').createServer()
   , io = require('socket.io').listen(app);
 
 app.listen(1337);
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
