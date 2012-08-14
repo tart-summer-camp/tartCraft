682b7e9ca0a0c56484f8807a85be89364d38fc2a
Umut Kaya Ilkiz <umutkaya2000@hotmail.com>
2012-08-14 19:41:04 +0300

    app.js user list with dummy user

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var app = require('express').createServer()
   , io = require('socket.io').listen(app);
 
 app.listen(1337);
+var userlist = [{username:"falan",password:"filan"}];
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
