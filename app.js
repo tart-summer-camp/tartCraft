384c4662a80e8a6cead7c7f15375d443eaab1645
denizece <aktandenizece89@gmail.com>
2012-08-16 13:24:18 +0300

    comment out kisimlar silindi (app.require)

--- app.js
+++ app.js
@@ -23,22 +23,6 @@ app.configure('production', function () {
     app.use(express.errorHandler());
 });
 
-
-/*var app = require('express').createServer()
- , routes = require('./routes')
- , io = require('socket.io').listen(app);
-
- app.configure(function(){
- app.set('views', __dirname + '/views');
- app.set('view engine', 'jade');
- // app.use(express.bodyParser());
- // app.use(express.methodOverride());
- app.use(app.router);
- //   app.use(express.static(__dirname + '/public'));
- });
-
-
- */
 app.listen(1337); // port number
 var races = {
     'mage':{
