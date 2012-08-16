202c9a1aa707b3bd14c4fd89c019b143f684acf3
Can Kutlu Kinay <ckkinay@github>
2012-08-16 21:07:14 +0300

    Cleaned up viewRenderer.js

--- viewRenderer.js
+++ viewRenderer.js
@@ -6,12 +6,14 @@
  * To change this template use File | Settings | File Templates.
  */
 exports.viewRenderer = function (pagename) {
-    //var that=this;
-   content={
-        sitecontent: ""
+    content = {
+        sitecontent:""
     };
+
     fs = require('fs');
+
     if (pagename == 'main') {
-       return fs.readFileSync('./views/main.html','utf8').toString();
+        return fs.readFileSync('./views/main.html', 'utf8').toString();
     }
 };
+
