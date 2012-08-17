652c21ddc070cb8d20507df295cf890f34066826
omurturan <omur.turann@gmail.com>
2012-08-17 11:06:13 +0300

    Added chat window autoscroll down functionality

--- public/js/mainPageSockets.js
+++ public/js/mainPageSockets.js
@@ -7,5 +7,7 @@
 tartCraft.mainPageSockets = function (socket) {
     socket.on('chatReceive', function (data) {
         $("#dialogBox").append("\n"+ data.username + " : " + data.message);
+        var dBox = $('#dialogBox');
+        dBox.scrollTop( dBox[0].scrollHeight - dBox.height() );
     });
 };
